5d1f6087d1a9bdcaa4be39b5a02620ee33f5832f
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-14 23:34:34 +0530

    update Login_form.fxml UI

--- src/main/java/edu/icet/controller/mainController/LogInController.java
+++ src/main/java/edu/icet/controller/mainController/LogInController.java
@@ -1,18 +1,73 @@
 package edu.icet.controller.mainController;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
-public class LogInController {
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogInController extends Component implements Initializable {
+
+    @FXML
+    private CheckBox checkSelect;
+
+    @FXML
+    private AnchorPane loginPane;
+
+    @FXML
+    private AnchorPane signUpPane;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private PasswordField txtPassword1;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private TextField txtUsername1;
 
     public void userNameOnAction(ActionEvent actionEvent) {
+        txtPassword.requestFocus();
     }
 
     public void checkPasswordOnAction(ActionEvent actionEvent) {
+        btnLogInOnAction(actionEvent);
     }
 
     public void btnLogInOnAction(ActionEvent actionEvent) {
     }
 
     public void btnSignInOnAction(ActionEvent actionEvent) {
+        signUpPane.setVisible(true);
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+        signUpPane.setVisible(false);
+    }
+
+    public void newUsernameOnAction(ActionEvent actionEvent) {
+        txtPassword1.requestFocus();
+    }
+
+    public void checkSecondPasswordOnAction(ActionEvent actionEvent) {
+        btnSignUpAction(actionEvent);
+    }
+
+    public void btnSignUpAction(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loginPane.setVisible(true);
+        signUpPane.setVisible(false);
     }
 }
