241ec6dc10ebe7f23b32b4d69e5a4b2faf19211b
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-14 22:41:03 +0530

    create Login_form.fxml UI

---
+++ src/main/java/edu/icet/controller/mainController/LogInController.java
@@ -0,0 +1,18 @@
+package edu.icet.controller.mainController;
+
+import javafx.event.ActionEvent;
+
+public class LogInController {
+
+    public void userNameOnAction(ActionEvent actionEvent) {
+    }
+
+    public void checkPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSignInOnAction(ActionEvent actionEvent) {
+    }
+}
