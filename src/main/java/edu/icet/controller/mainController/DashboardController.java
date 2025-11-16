9735fc232d0b09bad6309aea1d0a6390b00645ba
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-16 11:19:32 +0530

    create dashboard UI and dashboard controller

---
+++ src/main/java/edu/icet/controller/mainController/DashboardController.java
@@ -0,0 +1,42 @@
+package edu.icet.controller.mainController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class DashboardController {
+
+    @FXML
+    private AnchorPane mainContent;
+
+    @FXML
+    void btnCustomersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrdersOnAction(ActionEvent event) {
+
+    }
+
+}
