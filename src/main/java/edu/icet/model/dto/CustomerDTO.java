2ac01c6a36585090e430dbc8d5eeb23581aab1fa
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-16 21:54:09 +0530

    create customerDTO object

--- src/main/java/edu/icet/model/dto/CustomerDTO.java
+++ src/main/java/edu/icet/model/dto/CustomerDTO.java
@@ -1,21 +1,22 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDTO {
-    private final String id;
-    private final String name;
-    private final String address;
-    private final String contact;
-
-    public CustomerDTO(String id, String name, String address, String contact) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.contact = contact;
-    }
-
-    // Getters are required for the TableView to work
-    public String getId() { return id; }
-    public String getName() { return name; }
-    public String getAddress() { return address; }
-    public String getContact() { return contact; }
+    private String id;
+    private String title;
+    private String name;
+    private Date date;
+    private double salary;
+    private String address;
+    private String city;
+    private String province;
+    private String postalCode;
 }
