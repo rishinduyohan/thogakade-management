a44a29beacb36d5c0a5485f522df644759f92243
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-24 13:49:10 +0530

    create bill print form

---
+++ src/main/java/edu/icet/controller/BillController.java
@@ -0,0 +1,13 @@
+package edu.icet.controller;
+
+import javafx.event.ActionEvent;
+
+public class BillController {
+    public void btnPrintOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCloseOnAction(ActionEvent actionEvent) {
+
+    }
+
+}
